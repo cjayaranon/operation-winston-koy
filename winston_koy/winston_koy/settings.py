a7b6bd88b5297d5b40ac29121f6b946a1b9bcb8c
Harris Harijono <hpharijono@gmail.com>
2016-04-29 15:35:05 +0800

    added settings and urls

--- winston_koy/winston_koy/settings.py
+++ winston_koy/winston_koy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hackademy',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,5 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend','static')]
+
